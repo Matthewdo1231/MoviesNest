8bf95caa8f2317cdf583bef7bd326444576bee7c
matthewdo1231 <matthewdoarda@gmail.com>
2025-07-11 02:38:46 +0800

    feat:screen size fixed

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -4,7 +4,7 @@ import NavBarTags from './NavBarTags'
 
 const NavBar = () => {
   return (
-    <nav className='fixed z-10 flex lg:top-0 w-full h-[10rem] px-[4rem] lg:px-[20rem] bg-gradient-to-b from-black to-transparent'>
+    <nav className='fixed z-10 flex lg:top-0 w-full h-[10rem] px-[1rem] lg:px-[20rem] bg-gradient-to-b from-black to-transparent'>
      <ul className='flex lg:justify-between items-center w-full'>
           <img className='h-[6rem] lg:h-[8rem] mt-2' src={logo} alt="logo"/>
           <NavBarTags/>
